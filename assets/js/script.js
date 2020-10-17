0f2449fdc19351df494b36b03b058442a19289ef
shaggy <simmons5family@gmail.com>
2020-10-16 19:05:22 -0500

    hover highlight

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,6 +203,8 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+
     }
 }; 
 
@@ -219,7 +221,15 @@ var dropTaskHandler = function(event) {
     } else if (statusSelectEl === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
+}
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list")
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 }
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -227,3 +237,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandeler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
